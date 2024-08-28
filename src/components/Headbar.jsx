e38d1da8661928802c5b4816b3de5a6cbd5e53af
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-08-28 17:49:10 +0200

    new commit

--- src/components/Headbar.jsx
+++ src/components/Headbar.jsx
@@ -1,19 +1,28 @@
 import logoIcon from '../images/logo-icon.jpeg';
 import shoppingCartIcon from '../images/shopping-cart-icon.svg';
 import userIcon from '../images/user-icon.svg';
+import magnifyingGlass from '../images/magnifying-glass.svg';
+import {useState} from 'react';
 
 const Headbar = () => {
+    const [inputData, setInputData] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputData(event.target.value);
+    }
+
     return (
         <div className='headbar'>
             <div className='headbar'>
                 <img className='logo-icon' src={logoIcon} alt='logo glutennly' />
             </div>
-            <div className='headbar'>
-
+            <div className='headbar headbar-search-input'>
+                <input className='search-input' type='text' placeholder='Szukaj'/>
+                <button className='button' onClick={handleInputChange}><img className='headbar-icons' src={magnifyingGlass} alt='logo lupy' /></button>
             </div>
             <div className='headbar headbar-incons-box'>
-                <img className='headbar-icons' src={shoppingCartIcon} alt='logo wózka sklepowego' />
-                <img className='headbar-icons' src={userIcon} alt='logo użytkownika' />
+                <button className='button'><img className='headbar-icons' src={shoppingCartIcon} alt='logo wózka sklepowego' /></button>
+                <button className='button'><img className='headbar-icons' src={userIcon} alt='logo użytkownika' /></button>
             </div>
         </div>
     );
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -9,31 +9,33 @@ const ProductDetails = () => {
 			.catch((error) => console.log(error));
 	}, []);
 	return (
-		<div className='subpage-bgc'>
+		<div className='subpage-bg'>
 			<h1 className='subpage-header'>Szczegóły produktu</h1>
 			<div className='subpage-content'>
 				{product ? (
 					<table>
-						<tr>
-							<th>Nazwa</th>
-							<td>{product.name}</td>
-						</tr>
-						<tr>
-							<th>Id</th>
-							<td>{product.id}</td>
-						</tr>
-						<tr>
-							<th>Typ produktu</th>
-							<td>{product.productType}</td>
-						</tr>
-						<tr>
-							<th>Cena</th>
-							<td>{product.price}</td>
-						</tr>
-						<tr>
-							<th>Opis</th>
-							<td>{product.description}</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th>Nazwa</th>
+								<td>{product.name}</td>
+							</tr>
+							<tr>
+								<th>Id</th>
+								<td>{product.id}</td>
+							</tr>
+							<tr>
+								<th>Typ produktu</th>
+								<td>{product.productType}</td>
+							</tr>
+							<tr>
+								<th>Cena</th>
+								<td>{product.price}</td>
+							</tr>
+							<tr>
+								<th>Opis</th>
+								<td>{product.description}</td>
+							</tr>
+						</tbody>
 					</table>
 				) : (
 					<p>Ładowanie...</p>
